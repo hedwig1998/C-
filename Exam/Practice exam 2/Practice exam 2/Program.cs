36902f6013e9d4fd339349e1f430c242d5f914f2
hedwig1998 <133191261+hedwig1998@users.noreply.github.com>
2024-03-14 09:36:34 +0700

    Add files via upload

---
+++ Exam/Practice exam 2/Practice exam 2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_exam_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lion lion = new Lion(100, "Lion");
+            Tiger tiger = new Tiger(200, "Tiger");
+            lion.Show();
+            Console.WriteLine();
+
+            tiger.Show();
+        }
+    }
+
+    class Animal
+    {
+        protected double weight;
+        protected string name;
+
+        public Animal(double weight, string name)
+        {
+            this.weight = weight;
+            this.name = name;
+        }
+
+        public virtual void Show()
+        {
+            Console.WriteLine($"Name: {name}, Weight: {weight} kg");
+        }
+
+        public void SetMe(double weight, string name)
+        {
+            this.weight = weight;
+            this.name = name;
+        }
+    }
+
+    class Lion : Animal
+    {
+        public Lion(double weight, string name) : base(weight, name)
+        {
+        }
+
+        public override void Show()
+        {
+            base.Show();
+        }
+    }
+
+    class Tiger : Animal
+    {
+        public Tiger(double weight, string name) : base(weight, name)
+        {
+        }
+
+        public override void Show()
+        {
+            base.Show();
+        }
+    }
+}
---
+++ Exam/Practice exam/Practice exam/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cylinder cylinder = new Cylinder();
+            Console.WriteLine("Enter the dimenstions of the cylinder:");
+            Console.Write("Radius: ");
+            double radius = double.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            double height = double.Parse(Console.ReadLine());
+            cylinder.SetDimensions(radius, height);
+
+            cylinder.Process();
+
+            cylinder.Result();
+        }
+    }
+    class Cylinder
+    {
+        double Radius { get; set; }
+        double Height { get; set; }
+        double BaseArea { get; set; }
+        double LateralArea { get; set; }
+        double TotalArea { get; set; }
+        double Volume { get; set; }
+        public void SetDimensions(double radius, double height)
+        {
+            Radius = radius;
+            Height = height;
+        }
+
+        public void Process()
+        {
+            BaseArea = Radius * Radius * Math.PI;
+            LateralArea = 2 * Math.PI * Radius * Height;
+            TotalArea = 2 * Math.PI * Radius * (Height + Radius);
+            Volume = Math.PI * Radius * Radius * Height;
+        }
+
+        public void Result()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("Cylinder Characteristics");
+            Console.WriteLine($"Radius: {Radius}");
+            Console.WriteLine($"Height: {Height}");
+            Console.WriteLine($"Base Area: {BaseArea}");
+            Console.WriteLine($"Lateral Area: {LateralArea}");
+            Console.WriteLine($"Total Area: {TotalArea}");
+            Console.WriteLine($"Volume: {Volume}");
+        }
+    }
+}
